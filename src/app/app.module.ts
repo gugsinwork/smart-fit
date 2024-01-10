22a18ddf83771c4bb4dbb0342cd037f52872bc4a
gugs(Gustavo.L) <123033640+gugsinwork@users.noreply.github.com>
2024-01-10 17:56:24 -0300

    ADD: pages

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FormComponent } from './components/form/form.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FormComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
